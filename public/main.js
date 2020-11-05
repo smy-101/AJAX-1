35383818df3509d181b9cc8fe2e6f149091bdae8
smy-101 <shimy1011@foxmail.com>
2020-11-05 23:22:19 +0800

    初步了解AJAX

---
+++ public/main.js
@@ -0,0 +1,106 @@
+getCSS.onclick = () => {
+    const request = new XMLHttpRequest();
+    request.open("GET", "/style.css"); // readyState = 1
+    request.onreadystatechange = () => {
+        console.log(request.readyState);
+        // 下载完成，但不知道是成功 2xx 还是失败 4xx 5xx
+        if (request.readyState === 4) {
+            if (request.status >= 200 && request.status < 300) {
+                // 创建 style 标签
+                const style = document.createElement("style");
+                // 填写 style 内容
+                style.innerHTML = request.response;
+                // 插到头里面
+                document.head.appendChild(style);
+            } else {
+                alert("加载 CSS 失败");
+            }
+        }
+    };
+    request.send(); // readyState = 2
+};
+
+getJS.onclick = () => {
+    const request = new XMLHttpRequest()
+    request.open('Get', '/2.js')
+    request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+            if (request.status >= 200 && request.status < 300) {
+                const script = document.createElement("script");
+                script.innerHTML = request.response;
+                document.body.appendChild(script);
+            } else {
+                alert("加载 JS 失败");
+            }
+        }
+    };
+    request.send();
+}
+
+getHTML.onclick = () => {
+    const request = new XMLHttpRequest()
+    request.open('Get', '/3.html')
+    request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+            if (request.status >= 200 && request.status < 300) {
+                const div = document.createElement("div");
+                div.innerHTML = request.response;
+                document.body.appendChild(div);
+            } else {
+                alert("加载 HTML 失败");
+            }
+        }
+    };
+    request.send();
+}
+
+getXML.onclick = () => {
+    const request = new XMLHttpRequest()
+    request.open('Get', '/4.xml')
+    request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+            if (request.status >= 200 && request.status < 300) {
+                const dom = request.responseXML;
+                const text = dom.getElementsByTagName("warning")[0].textContent;
+                console.log(text.trim());
+            } else {
+                alert("加载 XML 失败");
+            }
+        }
+    };
+    request.send();
+}
+
+getJSON.onclick = () => {
+    const request = new XMLHttpRequest();
+    request.open("get", "/5.json");
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(typeof request.response);
+            console.log(request.response);
+            const bool = JSON.parse(request.response);
+            console.log(typeof bool);
+            console.log(bool);
+        }
+    };
+    request.send();
+};
+
+
+let n = 1;
+getPage.onclick = () => {
+    const request = new XMLHttpRequest();
+    request.open("GET", `/page${n + 1}`);
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200) {
+            const array = JSON.parse(request.response);
+            array.forEach(item => {
+                const li = document.createElement("li");
+                li.textContent = item.id;
+                xxx.appendChild(li);
+            });
+            n += 1
+        }
+    };
+    request.send();
+};
